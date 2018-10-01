384b51a58618dd6a513fa351de2e425c6cdcaa28
yubin <byu@xiaohongshu.com>
2018-10-01 11:16:39 +0800

    增加dev配置

--- shopcloud-gateway/src/main/java/com/maiya/shopcloud/gateway/ShopCloudGatewayApplication.java
+++ shopcloud-gateway/src/main/java/com/maiya/shopcloud/gateway/ShopCloudGatewayApplication.java
@@ -3,9 +3,6 @@ package com.maiya.shopcloud.gateway;
 import com.didispace.swagger.butler.EnableSwaggerButler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,10 +15,10 @@ import org.springframework.web.filter.CorsFilter;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableSwaggerButler
-public class PaasCloudGatewayApplication {
+public class ShopCloudGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaasCloudGatewayApplication.class, args);
+		SpringApplication.run(ShopCloudGatewayApplication.class, args);
 	}
 
 	@Bean
